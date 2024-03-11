d8b9914adaa088f2333845a477e160a573237960
andrey-trasov <159887936+andrey-trasov@users.noreply.github.com>
2024-03-11 16:33:16 +0300

    exercise_1

---
+++ utils/classes.py
@@ -0,0 +1,19 @@
+class Category:
+
+    def __init__(self, name, description, goods):
+        self.name = name                   #название
+        self.description = description     #описание
+        self.goods = goods                 #товары
+
+
+
+
+class Category:
+
+    def __init__(self, name, description, price, quantity_in_stock):
+        self.name = name                             #название
+        self.description = description               #описание
+        self.price = price                           #цена
+        self.quantity_in_stock = quantity_in_stock   #количество в наличии
+
+
