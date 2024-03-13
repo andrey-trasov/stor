6958b647f1415851f512044890eeecc21d50256a
andrey-trasov <159887936+andrey-trasov@users.noreply.github.com>
2024-03-13 21:55:36 +0300

    stor 13.1

--- utils/main.py
+++ utils/main.py
@@ -6,14 +6,13 @@ def main():
     global i
     file_json = 'products.json'
     file = open_file(file_json)
-    number_of_categories = len(file)
     list_category = []
     for i in file:
         list_product = []
         for j in i["products"]:
             product = Product(j["name"], j["description"], j["price"], j["quantity"])
             list_product.append(product)  # список классов Product
-        category = Category(i['name'], i['description'], list_product, number_of_categories)
+        category = Category(i['name'], i['description'], list_product)
         list_category.append(category)  # список классов Category
     # печатаем данные из списка Category
 
@@ -21,15 +20,15 @@ def main():
         print(f'Название категории: {i.name}')
         print(f'Описание категории: {i.description}')
         print("Товары:")
-        for j in i.goods:
+        for j in i.products:
             print(f'Название товара: {j.name}')
             print(f'Описание товара: {j.description}')
             print(f'Цена товара: {j.price}')
             print(f'Количество в наличии: {j.quantity}')
             print()
         print()
-    print(f'Общее количество категорий: {i.number_of_categories}')
-    print(f'Количество уникальных продуктов: {i.number_of_unique_products}')
+    print(f'Общее количество категорий: {i.category_count}')
+    print(f'Количество уникальных продуктов: {i.product_count}')
 
 
 if __name__ == '__main__':
