d315c99021e62fb739016aa3cde5eeae9dbb63b7
andrey-trasov <159887936+andrey-trasov@users.noreply.github.com>
2024-03-16 12:47:54 +0300

    stor 13.2

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -71,7 +71,6 @@ def category():
 def test_init_category(category):
     assert category.name == "Смартфоны"
     assert category.description == "Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни"
-    assert category.products == commodity
     assert category.category_count == 1
     assert category.product_count == 3
 
@@ -86,3 +85,15 @@ def test_init_product(product):
     assert product.description == "256GB, Серый цвет, 200MP камера"
     assert product.price == 180000.0
     assert product.quantity == 5
+
+@pytest.fixture()
+def dictionary():
+    return Product.from_dictionary(commodity[0])
+
+def test_class_product(dictionary):
+    assert dictionary.name == "Samsung Galaxy C23 Ultra"
+    assert dictionary.description == "256GB, Серый цвет, 200MP камера"
+    assert dictionary.price == 180000.0
+    assert dictionary.quantity == 5
+
+
--- utils/classes.py
+++ utils/classes.py
@@ -2,13 +2,22 @@ class Category:
     category_count = 0
     product_count = 0
 
-    def __init__(self, name: str, description: str, products: list = None) -> None:
+    def __init__(self, name: str, description: str, products: list = None):
         self.name = name                              #название
         self.description = description                #описание
-        self.products = products                      #список товаров
+        self.__products = products                    #список товаров
         Category.category_count += 1                  #общее количество категорий
-        Category.product_count += len(self.products)  #количество уникальных продуктов
+        Category.product_count += len(self.__products)  #количество уникальных продуктов
 
+
+    def add_products(self, goods):
+        self.__products.append(goods)
+
+
+    @property
+    def products(self):
+        for j in self.__products:
+            print(f"{j.name}, {int(j.price)} руб. Остаток: {j.quantity} шт.")
 
 
 
@@ -17,7 +26,35 @@ class Product:
     def __init__(self, name: str, description: str, price: float, quantity: int):
         self.name = name                              #название
         self.description = description                #описание
-        self.price = float(price)                     #цена
+        self.__price = float(price)                   #цена
         self.quantity = quantity                      #количество в наличии
+
+
+    @classmethod
+    def from_dictionary(cls, dictionary):
+        name = dictionary["name"]
+        description = dictionary["description"]
+        price = dictionary["price"]
+        quantity = dictionary["quantity"]
+        return cls(name, description, price, quantity)
+
+
+    @property
+    def price(self):
+        return self.__price
+
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= self.__price:
+            print('Цена введена некорректная')
+        else:
+            self.__price = new_price
+
+
+    @price.deleter
+    def price(self):
+        self.__price = None
+
 
 
